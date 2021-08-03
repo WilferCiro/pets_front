7ff859ba0cc4c55b4d1bfd0643e0330912af0332
Wilfer Ciro <wilcirom@gmail.com>
2021-08-03 13:19:40 -0500

    Changes

---
+++ components/ConstantServer.js
@@ -0,0 +1,10 @@
+const ConstantServer = {
+	URL_public        : '/api/public/',
+	URL_private       : '/api/private/',
+	URL_login         : '/api/login/',
+	URL_server        : 'http://kiwipyme.pythonanywhere.com',
+	//URL_server        : 'http://127.0.0.1:8000',
+	URL_recover       : '/api/public/recover/'
+};
+
+export default ConstantServer;
